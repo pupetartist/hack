3c1dc9a98c97d9a4131900d66bdb1cfc593a8c3c
angeloLuna <angel.luna.najera@gmail.com>
2016-03-06 01:41:31 -0600

    reinicio de landing page

--- app/app.js
+++ app/app.js
@@ -2,6 +2,32 @@
   'use strict';
 
   angular
-    .module('segundamano',['ngRoute','ngResource']);
+    .module("llegaprimero", [])
+    .directive("cardOne", cardOne);
+
+  function cardOne(){
+      var directive = {
+        restrict: 'EA',
+        templateUrl: '/partials/cardone.html',
+        controller:   oneController,
+        controllerAs: 'one',
+        replace : true
+      };
+      return directive;
+  }
+
+  oneController.$inject= ["$scope"];
+
+  function oneController($scope){
+    var one = this;
+
+    one.list = [
+      {'id':'MX-HID', 'name':'Hidalgo'},
+      {'id':'MX-AGU', 'name':'Aguascalientes'},
+      {'id':'MX-CHH',  'name':'Chihuahua'},
+      {'id':'MX-PUE', 'name':'Puebla'},
+    ];
+    one.over = null;
+  }
 
 })();
